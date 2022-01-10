a5b32d5479760a2e71daeb766b1f1daec7922ff2
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2022-01-10 17:36:26 +0100

    Really remove platform dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ include("ccbench.core")
 include("ccbench.tiger")
 //include("ccbench.chocopy")
 //include("ccbench.webdsl")
-//include("ccbench.platform")
 
 //includeBuild("../devenv-220110")
 //includeBuild("../chocopy-grading-2021")
