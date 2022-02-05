126c8234e8ff23cf78c6b0aa4beaab71d208b780
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2022-02-05 16:53:36 +0100

    Improve deploy scripts

--- deploy-chocopy.sh
+++ deploy-chocopy.sh
@@ -10,13 +10,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 # Build and Copy CCBench
-echo "Building CCBench"
+echo "Building CCBench ChocoPy"
 ./gradlew installDist
-echo "Copying CCBench"
+echo "Copying CCBench ChocoPy"
 rsync -av ./ccbench.chocopy/build/install/ccbench.chocopy/ w2019:ccbench.chocopy
 
 # Copy the source project and generated tests
-echo "Copying source project"
+echo "Copying ChocoPy source project"
 rsync -av ~/repos/ccbench-chocopy/ w2019:chocopy-benchmark/ \
   --exclude .git \
   --exclude results
---
+++ deploy-tiger.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o noclobber
+set -o nounset
+set -o xtrace
+
+# Get the script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
+# Build and Copy CCBench
+echo "Building CCBench Tiger"
+./gradlew installDist
+echo "Copying CCBench Tiger"
+rsync -av ./ccbench.tiger/build/install/ccbench.tiger/ w2019:ccbench.tiger
+
+# Copy the source project and generated tests
+echo "Copying Tiger source project"
+rsync -av ~/repos/ccbench-tiger/ w2019:tiger-benchmark/ \
+  --exclude .git \
+  --exclude results
+
+echo "Done!"
+
+# Execute the tests using:
+#./ccbench.tiger/bin/ccbench.tiger \
+#  run tiger-w2019 \
+#  -p tiger-benchmark/project/ \
+#  -i tiger-benchmark/cases/Tiger.yml \
+#  -o tiger-benchmark/results/ \
+#  -w 100 \
+#  --seed 12345
